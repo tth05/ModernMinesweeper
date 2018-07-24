71dbcb4cf6ccbaa66cd41f5ce8234db62ebe48d8
KotlinIsCool <ichmagbagga@gmail.com>
2018-07-24 20:45:33 +0200

    Initial commit

---
+++ src/com/github/kotliniscool/Main.java
@@ -0,0 +1,22 @@
+package com.github.kotliniscool;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FlowPane root = new FlowPane(5, 5);
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
